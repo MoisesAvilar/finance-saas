f3fb9924b98dcc3e39d713afb851aaf6666f2420
Moisés <moiseavilar0@gmail.com>
2025-12-25 22:53:12 -0300

    feat: update service worker cache logic to include specific files

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,17 +1,30 @@
 var staticCacheName = "django-pwa-v1";
 
+var filesToCache = [
+    '/static/images/favicon.ico',
+    '/static/images/android-chrome-192x192.png', 
+    '/static/images/android-chrome-512x512.png',
+    '/static/images/apple-touch-icon.png',
+];
+
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(function (response) {
+        return caches.open(staticCacheName).then(function (cache) {
+             return response;
+        });
+      })
+      .catch(function () {
+        return caches.match(event.request);
+      })
   );
 });
